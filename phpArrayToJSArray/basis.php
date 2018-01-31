11e3d74ee5e77282a2e2b47febf2eef8a620a293
Leon <leonscherweit@Leons-MBP.fritz.box>
2018-01-31 20:24:22 +0100

    PHP zu Javascript Basisfunktionalität

---
+++ phpArrayToJSArray/basis.php
@@ -0,0 +1,12 @@
+<?php $MatrNr = ["21432425", "21452523", "11521342"];
+?>
+
+<script type="text/javascript">
+
+    var jArray= <?php echo json_encode($MatrNr ); ?>;
+
+    for(var i=0;i<jArray.length;i++){
+        alert(jArray[i]);
+    }
+
+ </script>

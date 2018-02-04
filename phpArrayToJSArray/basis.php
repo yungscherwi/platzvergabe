b5a1654db17c7b08ae5d39b1c3ad42180b6061d9
Peek009 <kallisch2000@googlemail.com>
2018-02-04 15:00:51 +0100

    test
    test

--- phpArrayToJSArray/basis.php
+++
@@ -1,9 +0,0 @@
-<?php $MatrNr = ["21432425", "21452523", "11521342"];
-?>
-
-<script type="text/javascript">
-    var jArray= <?php echo json_encode($MatrNr ); ?>;
-    for(var i=0;i<jArray.length;i++){
-        alert(jArray[i]);
-    }
- </script>

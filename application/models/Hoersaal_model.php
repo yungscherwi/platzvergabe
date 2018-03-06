0fe91c899740ed0eaf33ab281f1ec772c04b15f3
Leon <leonscherweit@Leons-MacBook-Pro.local>
2018-03-06 12:02:57 +0100

    hoersaal_model kommentiert

--- application/models/Hoersaal_model.php
+++ application/models/Hoersaal_model.php
@@ -6,7 +6,6 @@ class Hoersaal_model extends CI_Model{
 
 
   //Checkt ob Sperrplätze vorhanden im jeweiligen Raum
-
   public function get_sperrplatzCheck($raum){
     $sql = "SELECT sperrplaetze FROM hoersaaluebersicht WHERE hoersaalID = '$raum'"; //SQL-Abfrage für Platzanzahl
     $query = $this->db->query($sql);
@@ -16,7 +15,7 @@ class Hoersaal_model extends CI_Model{
     return ($arr);
   }
 
-// Ausgabe von Spalte 'platzAnzahl' in einem Array
+  // Ausgabe von Spalte 'platzAnzahl' in einem Array
   public function get_platzAnzahl($raum){
     $sql = "SELECT group_concat(platzAnzahl separator ',') as 'platzAnzahl' FROM hoersaele WHERE hoersaalID = '$raum'";
     $query = $this->db->query($sql);
@@ -27,6 +26,7 @@ class Hoersaal_model extends CI_Model{
 
     return($revarr);
   }
+
   //Ausgabe von Spalte 'reihe' in einem Array
   public function get_reihe($raum){
     $sql = "SELECT group_concat(reihe separator ',') as 'reihe' FROM hoersaele WHERE hoersaalID = '$raum'";
@@ -38,7 +38,7 @@ class Hoersaal_model extends CI_Model{
     return ($revarr);
   }
 
-//fragt die Anzahl der Plätze ab
+  //fragt die Anzahl der Plätze ab
   public function get_plaetze($raum){
     $sql = "SELECT plaetze FROM hoersaaluebersicht WHERE hoersaalID = '$raum'"; //SQL-Abfrage für Platzanzahl
     $query = $this->db->query($sql);
@@ -48,8 +48,9 @@ class Hoersaal_model extends CI_Model{
     return ($arr);
   }
 
+  //fragt die Gesamtzahl der Plätze eines Hörsaals für eine Klausur ab
   public function get_allPlaetze(){
-    $sql = "SELECT group_concat(plaetze separator ',') as 'plaetze' FROM `hoersaaluebersicht`"; // geht nur mit ` statt ' komischerweise
+    $sql = "SELECT group_concat(plaetze separator ',') as 'plaetze' FROM `hoersaaluebersicht`";
     $query = $this->db->query($sql);
     $array1 = $query->row_array();
     $arr = explode(',',$array1['plaetze']);
@@ -75,45 +76,44 @@ class Hoersaal_model extends CI_Model{
     $arr = explode(',',$array1['sperrplatz']);
     $revarr = array_reverse($arr); //für richtige Ausrichtung des hörsaals
 
-$zwischen=[];
-$new=[];
-$final=[];
-for($i=0;$i<count($revarr);$i++){
-	if(!empty($revarr[$i+1])){
-	if($sperrplatzreihe[$i]==$sperrplatzreihe[$i+1]){
-		array_push($zwischen,$revarr[$i]);
-	}
-	else{
-		array_push($zwischen,$revarr[$i]);
-		sort($zwischen);
-		array_push($final,$zwischen);
-		$zwischen=[];
-	   }
-	}
-	else{
-		if($sperrplatzreihe[$i]==$sperrplatzreihe[$i-1]){
-			array_push($zwischen,$revarr[$i]);
-			sort($zwischen);
-			array_push($final,$zwischen);
-		}
-		else{
-		sort($zwischen);
-		array_push($final,$zwischen);
-		$zwischen=[];
-		array_push($zwischen,$revarr[$i]);
-		array_push($final,$zwischen);
-	   }
-   }
-}
-
-  $result = call_user_func_array('array_merge', $final);
-
-
-    return ($result);
+    //Algorithmus um die Sperrplätze pro Reihe aufsteigend und nicht absteigend sortiert zu haben
+    $zwischen=[];
+    $new=[];
+    $final=[];
+    for($i=0;$i<count($revarr);$i++){
+	     if(!empty($revarr[$i+1])){
+	        if($sperrplatzreihe[$i]==$sperrplatzreihe[$i+1]){
+		          array_push($zwischen,$revarr[$i]);
+	         }
+	     else{
+		       array_push($zwischen,$revarr[$i]);
+		         sort($zwischen);
+		           array_push($final,$zwischen);
+		             $zwischen=[];
+	      }
+	     }
+	     else{
+		       if($sperrplatzreihe[$i]==$sperrplatzreihe[$i-1]){
+			          array_push($zwischen,$revarr[$i]);
+			             sort($zwischen);
+			                array_push($final,$zwischen);
+		       }
+		       else{
+		           sort($zwischen);
+		           array_push($final,$zwischen);
+		           $zwischen=[];
+		           array_push($zwischen,$revarr[$i]);
+		           array_push($final,$zwischen);
+	         }
+        }
+      }
+      //Merged zweidimesionalen Array in eindimensionalen
+      $result = call_user_func_array('array_merge', $final);
+      return ($result);
   }
   else{
     return;
-    }
+  }
 }
 
   public function get_revsperrplatz($raum){
@@ -145,7 +145,7 @@ for($i=0;$i<count($revarr);$i++){
     $this->db->insert('hoersaaluebersicht', $data);
   }
 
-    //Datenbank ist erstellt, jetzt Insert
+    //Insert des Hörsaals in die hoersaele Tabelle
     public function insertIntoDatabase($insertInfo){
     $infos = array(
       'platzAnzahl' => $insertInfo [1],
@@ -155,17 +155,17 @@ for($i=0;$i<count($revarr);$i++){
     $this->db->insert('hoersaele', $infos);
 
     return ($insertInfo);
+    }
 
-  }
-
+    //Anzahl der Plaetze wird in Tabelle 'hoersaal' ergänzt
     public function insertIntoHoersaal($hoersaalInfo){
-      //Anzahl der Plaetze wird in Tabelle 'hoersaal' ergänzt
       $sql = "UPDATE hoersaaluebersicht SET plaetze = ".$hoersaalInfo[1]." WHERE hoersaalID = '".$hoersaalInfo[0]."'";
       $query = $this->db->query($sql);
 
       return;
     }
 
+    //Einfügen ob Sperrplätze im jeweiligen Hörsaal vorhanden oder nicht
     public function updateSperrplatz($hoersaalInfo){
       //Sperrplätze auf 1 gesetzt, da vorhanden
       $sql = "UPDATE hoersaaluebersicht SET sperrplaetze = '1' WHERE hoersaalID = '".$hoersaalInfo[0]."'";
@@ -174,6 +174,7 @@ for($i=0;$i<count($revarr);$i++){
       return;
     }
 
+    //Einfügen der jeweiligen Sperrplätze in die sperrplaetze-Tabelle
     public function insertIntoSperrplaetze($sperrplatzInfo){
       $infos = array(
         'hoersaalID' => $sperrplatzInfo[0],
@@ -185,6 +186,7 @@ for($i=0;$i<count($revarr);$i++){
       return;
     }
 
+    //Löschen eines Hörsaals aus allen Tabellen
     public function delete_hoersaal($hoersaalID){
       $sql = "DELETE FROM hoersaele WHERE hoersaalID = '".$hoersaalID."'";
       $query = $this->db->query($sql);
@@ -198,7 +200,7 @@ for($i=0;$i<count($revarr);$i++){
 
       return;
     }
-//höchste Anzahl an Plätzen pro Reihe im Hörsaal
+    //maximale Anzahl an vorhandenen Plätzen in einer Reihe im jew. Hörsaal
     public function get_maxPlatzAnzahl($hoersaalID){
       $sql = "SELECT MAX(platzAnzahl) FROM hoersaele WHERE hoersaalID = '$hoersaalID'";
       $query = $this->db->query($sql);
